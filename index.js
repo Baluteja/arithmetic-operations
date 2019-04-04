e8fa7ca5a03241f6b4cde257a7ca95416dcc18d2
TRWS8macmini0340 <TRWS8macmini0340@balu.local>
2019-04-04 20:03:10 +0530

    new changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 "use strict";
 
 module.exports = {
+    // testing the sample for adding 2 numbers
     addTwoNumbers: function (first, second) {
         return first + second;
     },
-
+    // testing the sample for subtracting 2 numbers
     subtractTwoNumbers: function (first, second) {
         return first - second;
     },
-    
+    // testing the sample for multiplying 2 numbers
     multiplyTwoNumbers: function (first, second) {
         return first * second;
     }
