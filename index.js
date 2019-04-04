20b4c253f933911d2e2364a2f5ee1a030a955c72
TRWS8macmini0340 <TRWS8macmini0340@balu.local>
2019-04-04 18:47:12 +0530

    changes in readme file

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-function addTwoNumbers(a, b) {
-    return a+b;
-}
-function subtractTwoNumbers(a, b) {
-    return a-b;
-}
-function mulitplyTwoNumbers(a, b) {
-    return a*b;
+"use strict";
+
+module.exports = function acronym(sentence, callback) {
+  return new Promise((resolve, reject) => {
+    if (sentence.length === 0) {
+      reject('String, Please! e.g. "for your information"')
+      return callback('String, Please! e.g. "for your information"')
+    } else if (typeof sentence === 'function') {
+      callback = sentence
+
+      reject('¯\\_(ツ)_/¯')
+      return callback('¯\\_(ツ)_/¯')
+    } else {
+      const words = sentence.split(' ')
+      const acronym = words.map((word) => word[0])
+
+      resolve(acronym.join(''))
+      return callback(null, acronym.join(''))
+    }
+  })
 }
