4cab6951db182e64f0fdb549f9abf978ad5a2e40
TRWS8macmini0340 <TRWS8macmini0340@balu.local>
2019-04-04 18:09:47 +0530

    changes in readme file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arithmetic-operations
+
+[![Version npm](https://img.shields.io/badge/npm-version1.0.0-blue.svg)](https://www.npmjs.com/package/sample-arithmetic-operations)
---
+++ index.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(a, b) {
+    return a+b;
+}
+function subtractTwoNumbers(a, b) {
+    return a-b;
+}
+function mulitplyTwoNumbers(a, b) {
+    return a*b;
+}
